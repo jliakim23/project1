d9e77bd3e14a722ec7ca369c33ec7a3b8bafe914
Lia Kim <j.liakim23@gmail.com>
2023-07-10 19:41:56 -0400

    final touches

--- project.js
+++ project.js
@@ -5,7 +5,7 @@ let artistData, userInput;
 const BASE_URL = 'https://app.ticketmaster.com/discovery/v2/events.json?keyword=';
 const apikey = '48P0yZ4Lssx5vR5CCsSJTCnXeIkFTNnU';
 
-// Selecting DOM elements
+// Selecting DOM elements and assigning them to variables
 const $date = $('#Date');
 const $venue = $('#Venue');
 const $price = $('#Price');
@@ -15,9 +15,10 @@ const $form = $('form');
 const $image = $('#artistImage');
 const $info = $('#Info');
 
-
+//Event listeners, search button
 $form.on('submit', searchForEvents);
 
+//Function, goes through the ticketmaster api to pull data
 function searchForEvents(event) {
   event.preventDefault();
   userInput = $input.val();
@@ -38,6 +39,7 @@ function searchForEvents(event) {
   );
 }
 
+//render function is used to get specific properties of the artistData
 function render() {
   $date.text(artistData._embedded.events[0].dates.start.localDate); 
   $time.text(artistData._embedded.events[0].dates.start.localTime);
@@ -46,6 +48,9 @@ function render() {
   $image.attr('src', artistData._embedded.events[0].images[0].url);
   $info.text(artistData._embedded.events[0].name);
 
+
+  //website link, <a> element is used, wraps with button, appends with id #website
+ //purchase tickets
   const websiteLink = artistData._embedded.events[0].url;
   if (websiteLink) {
     const $websiteElement = $('<a>').attr('href', websiteLink).text('Purchase Tickets');
